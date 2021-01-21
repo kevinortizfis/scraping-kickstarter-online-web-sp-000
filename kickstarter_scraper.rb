c961c6b2a6339cd529a74a3347d6705c5dcf7ac3
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2021-01-21 00:44:02 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -4,7 +4,7 @@ require 'Nokogiri'
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-  kickstarter.css("project-card")
+  kickstarter.css("project-grid-4")
 end
 
 # :projects => {
