0520ec623059a045bdeb5e035785a5cff1d62a53
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2021-01-21 00:53:07 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,10 +11,11 @@ def create_project_hash
   # Iterate through the projects
   kickstarter.css("li.project.grid_4").each do |project|
     projects[project] = {}
-    puts projects
+
 end
-create_project_hash
-end
+
+projects
+
 # projects: kickstarter.css("li.project.grid_4")
 # title: project.css("h2.bbcard_name strong a").text
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
