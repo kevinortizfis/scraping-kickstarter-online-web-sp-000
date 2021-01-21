045c0ab7eb39e854f36fbc02876742c27cb19a45
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2021-01-21 00:50:10 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,7 +11,9 @@ end
 
 create_project_hash
 # projects: kickstarter.css("li.project.grid_4")
-# title: project.css("h2.bbcard_name strong a").text# :projects => {
+# title: project.css("h2.bbcard_name strong a").text
+# image link: project.css("div.project-thumbnail a img").attribute("src").value
+# :projects => {
 #   "My Great Project"  => {
 #     :image_link => "Image Link",
 #     :description => "Description",
