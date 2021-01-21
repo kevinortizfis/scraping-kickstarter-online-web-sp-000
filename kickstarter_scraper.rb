269027a963bee08e5bb0dfdeb9db1e40e53c99c5
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2021-01-21 00:38:08 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,22 @@
 # require libraries/modules here
+require 'Nokogiri'
+require 'Open-Uri'
 
 def create_project_hash
   # write your code here
 end
+
+# :projects => {
+#   "My Great Project"  => {
+#     :image_link => "Image Link",
+#     :description => "Description",
+#     :location => "Location",
+#     :percent_funded => "Percent Funded"
+#   },
+#   "Another Great Project" => {
+#     :image_link => "Image Link",
+#     :description => "Description",
+#     :location => "Location",
+#     :percent_funded => "Percent Funded"
+#   }
+# }
